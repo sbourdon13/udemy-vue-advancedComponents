37586af6e45262b6d9da77021ab1caa829872050
Sara <sara.bourdon@outlook.com>
2019-01-30 11:29:15 +0100

    Exercise 8 setup

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <!-- <button @click="selectedComponent = 'appQuote'">Quote</button>
         <button @click="selectedComponent = 'appAuthor'">Author</button>
         <button @click="selectedComponent = 'appNew'">New</button>
         <hr>
@@ -11,28 +11,41 @@
           <component :is="selectedComponent">
             <p>Default content</p>
           </component>
-        </keep-alive>
+        </keep-alive>-->
+        <br>
+        <button class="btn btn-primary">Load Blue Template</button>
+        <button class="btn btn-success" style="margin: 0 20px">Load Green Template</button>
+        <button class="btn btn-danger">Load Red Template</button>
+        <hr>
+        <app-blue></app-blue>
+        <app-green></app-green>
+        <app-red></app-red>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Quote from "./components/Quote";
-import Author from "./components/Author";
-import New from "./components/New";
+// import Quote from "./components/Quote";
+// import Author from "./components/Author";
+// import New from "./components/New";
+import Blue from "./components/Blue.vue";
+import Green from "./components/Green.vue";
+import Red from "./components/Red.vue";
 
 export default {
   data() {
     return {
-      quoteTitle: "The Quote",
       selectedComponent: "appQuote"
     };
   },
   components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
+    // appQuote: Quote,
+    // appAuthor: Author,
+    // appNew: New,
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red
   }
 };
 </script>
