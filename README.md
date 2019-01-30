aaad84c21ffdc892df1ef5ea80f09ae5645fc39d
Sara <sara.bourdon@outlook.com>
2019-01-30 11:23:51 +0100

    discover dynamic components

--- README.md
+++ README.md
@@ -1,26 +1,22 @@
-# advanced-components
+# Advanced-components
+
+This projects consists in small exercises to practice some advanced VueJS components features such as slots and dynamic components.
+The exercises come from [this Udemy course](https://www.udemy.com/vuejs-2-the-complete-guide/) by Maximilian Schwarzmüller.
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
-```
-
-### Run your tests
-```
-npm run test
-```
-
-### Lints and fixes files
-```
-npm run lint
 ```
--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <app-quote>
-          <h2 slot="title">{{ quoteTitle }}</h2>
-          <p>A wonderful quote</p>
-          <p>Nice author</p>
-        </app-quote>
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr>
+        <p>{{ selectedComponent }}</p>
+        <keep-alive>
+          <component :is="selectedComponent">
+            <p>Default content</p>
+          </component>
+        </keep-alive>
       </div>
     </div>
   </div>
@@ -14,15 +19,20 @@
 
 <script>
 import Quote from "./components/Quote";
+import Author from "./components/Author";
+import New from "./components/New";
 
 export default {
   data() {
     return {
-      quoteTitle: "The Quote"
+      quoteTitle: "The Quote",
+      selectedComponent: "appQuote"
     };
   },
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }
 };
 </script>
